5db227a3ff521a95aad852fe33f2c66c8f3a3251
tr1sm0s1n <mobinm.mohanan@gmail.com>
2024-02-17 20:25:28 +0530

    🔨 | added script to check

---
+++ checker.sh
@@ -0,0 +1,17 @@
+#!/usr/bin/env bash
+
+checker() {
+    if ! command -v $1 &> /dev/null; then
+        echo -e "\033[91m[❌] Installed $1\033[95m"
+    else
+        version=$($1 $2)
+        echo -e "\033[92m[✅] Installed $1 ($version)\033[0m"
+    fi
+}
+
+checker "code" "-v"
+checker "git" "-v"
+checker "node" "-v"
+checker "java" "--version"
+checker "besu" "--version"
+checker "teku" "--version"
