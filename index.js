4c2ca57a096ee20d1bc1dda09a0e30dd5bc88833
tr1sm0s1n <mobinm.mohanan@gmail.com>
2024-03-04 17:08:07 +0530

    ♻️ | removed function to use top-level await, updated script

--- index.js
+++ index.js
@@ -2,48 +2,48 @@ import { readFileSync } from "fs";
 import solc from "solc";
 import { JsonRpcProvider, ContractFactory } from "ethers";
 
-(async () => {
-  const provider = new JsonRpcProvider("http://127.0.0.1:8545");
-  const signer = await provider.getSigner();
+const provider = new JsonRpcProvider("http://127.0.0.1:8545");
+const signer = await provider.getSigner();
 
-  const balanceBefore = await provider.getBalance(signer.address);
-  console.log("Balance Before:", balanceBefore);
+const balanceBefore = await provider.getBalance(signer.address);
+console.log("Balance Before:", balanceBefore);
 
-  const CONTRACT_FILE = "contracts/Storage.sol";
+const CONTRACT_FILE = "contracts/Storage.sol";
 
-  const content = readFileSync(CONTRACT_FILE).toString();
+const content = readFileSync(CONTRACT_FILE).toString();
 
-  const input = {
-    language: "Solidity",
-    sources: {
-      [CONTRACT_FILE]: {
-        content: content,
+const input = {
+  language: "Solidity",
+  sources: {
+    [CONTRACT_FILE]: {
+      content: content,
+    },
+  },
+  settings: {
+    outputSelection: {
+      "*": {
+        "*": ["*"],
       },
     },
-    settings: {
-      outputSelection: {
-        "*": {
-          "*": ["*"],
-        },
-      },
-    },
-  };
+  },
+};
 
-  const compiled = solc.compile(JSON.stringify(input));
-  const output = JSON.parse(compiled);
+const compiled = solc.compile(JSON.stringify(input));
+const output = JSON.parse(compiled);
 
-  const abi = output.contracts[CONTRACT_FILE].Storage.abi;
-  const bytecode = output.contracts[CONTRACT_FILE].Storage.evm.bytecode.object;
+const abi = output.contracts[CONTRACT_FILE].Storage.abi;
+const bytecode = output.contracts[CONTRACT_FILE].Storage.evm.bytecode.object;
 
-  const factory = new ContractFactory(abi, bytecode, signer);
-  const contract = await factory.deploy();
+const factory = new ContractFactory(abi, bytecode, signer);
+const contract = await factory.deploy();
 
-  const trx = await contract.store("Hello, KBA!");
-  console.log("Transaction Hash:", trx.hash);
+console.log("Contract deployed:", await contract.getAddress());
 
-  const message = await contract.retrieve();
-  console.log("Message:", message);
+const trx = await contract.store("Hello, KBA!");
+console.log("Transaction Hash:", trx.hash);
 
-  const balanceAfter = await provider.getBalance(signer.address);
-  console.log("Balance After:", balanceAfter);
-})();
+const message = await contract.retrieve();
+console.log("Message:", message);
+
+const balanceAfter = await provider.getBalance(signer.address);
+console.log("Balance After:", balanceAfter);
